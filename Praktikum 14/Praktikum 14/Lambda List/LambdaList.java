a493b4ec15d4c3700ff2fe713a381aaafffdb77e
Rayyisbdp <118786667+Rayyisbdp@users.noreply.github.com>
2024-06-10 23:43:29 +0700

    Add files via upload

---
+++ Praktikum 14/Praktikum 14/Lambda List/LambdaList.java
@@ -0,0 +1,17 @@
+//Nama    :Muhammad Rayyis Budi Prasetyo
+//NIM     :24060122140112
+
+import java.util.ArrayList;
+
+public class LambdaList {
+    public static void main(String [] args) {
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add("Adi");
+        mahasiswaList.add("Bambang");
+        mahasiswaList.add("Cici");
+        mahasiswaList.add("Didi ");
+
+        //lambda digunakan sebagai parameter
+        mahasiswaList.forEach((nama) -> System.out.println(nama));
+    }
+}
